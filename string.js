2f1d9732f166cd7a91f8f23cb529eabbe7884e52
Mayankdhote18 <mayankdhote4316@gmil.com>
2025-09-25 20:27:18 +0530

    feat: add initial string manipulation and boolean conversion examples

---
+++ string.js
@@ -0,0 +1,19 @@
+let score ='true'
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+//"33abc" => NaN
+// true  => 1
+// false => 0
+let isLoggedIn = "rohan";
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn);   // true
+console.log(typeof BooleanIsLoggedIn); // boolean
